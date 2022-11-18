e63e971ff2e83fa4cd2473d3fc8c09a96e412db5
Sohini Bonthala <sohinibonthala@Sohinis-MacBook-Pro.local>
2022-11-17 21:05:25 -0800

    Add User login backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
-
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 // router.post('/test', function(req, res) {
 //     res.json({ requestBody: req.body });
 //   });
@@ -21,7 +22,13 @@ const router = require('express').Router();
 const { restoreUser } = require('../../utils/auth.js');
 
 router.use(restoreUser);
+router.use('/session', sessionRouter);
 
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
 // router.get(
 //   '/restore-user',
 //   (req, res) => {
